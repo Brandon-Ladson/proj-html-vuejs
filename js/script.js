920d290fa97f0cac546ae44a33af2d7efdbb62e5
Brandon Ladson <brandonladson1999@gmail.com>
2020-12-09 18:53:02 +0100

    creazione regole css e struttura dati js per footer

--- js/script.js
+++ js/script.js
@@ -6,6 +6,27 @@ var app = new Vue({
       'COURSES',
       'PAGES',
       'APP'
+    ],
+    linkCompanyFooter: [
+      'Register',
+      'Activate',
+      'All Courses',
+      'Groups Directory',
+      'Members Directory'
+    ],
+    linkCoursesFooter: [
+      'Design Dynamics',
+      'Internet of Things Security',
+      'Deep Virtual Reality',
+      'Artificial Intelligence',
+      'Machine Learning Toolsy'
+    ],
+    linkCategoriesFooter: [
+      'Register',
+      'Activate',
+      'All Courses',
+      'Groups Directory',
+      'Members Directory'
     ]
   }
 
