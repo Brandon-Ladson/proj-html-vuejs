3cc468c4e2665907377da2fbc65e16ff5cd4a0d4
Brandon Ladson <brandonladson1999@gmail.com>
2020-12-11 01:45:40 +0100

    cabio di struttura dati js 2 per aggiungere con vue anche logo e icone a navbar e footer

--- js/script.js
+++ js/script.js
@@ -1,12 +1,17 @@
 var app = new Vue({
   el: '#app',
   data: {
+    logo: 'img/logo.png',
+    searchIcon: 'fas fa-search',
+    cartIcon: 'fas fa-shopping-cart',
+    login: 'LOGIN',
     linkHeader: [
       'HOME',
       'COURSES',
       'PAGES',
       'APP'
     ],
+    greatApp: 'We make great app.',
     categorieFooter: [
       {
         nome: 'About Company',
